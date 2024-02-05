b5b51f5043a67c5f32a0ed4fa92c6dc6d4f99186
Rusi Papazov <rusi.papazov@jiminny.com>
2024-02-05 17:46:41 +0200

    copy index and json files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.ts',
@@ -18,4 +20,14 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'api', to: 'api' },
+            ],
+        }),
+    ],
 };
